5f429b1b908a598ea9f730d55805eb5858329f1a
David Lewis <david.lewis@hiveempire.com>
2016-08-12 13:11:09 +1000

    Friday session

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'React';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          onClick={() => this.props.selectBook(book) }
+          key={book.title}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
@@ -19,10 +26,19 @@ class BookList extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    books: state.books
-  };
+function mapStateToProps({ books }) {
+  return { books };
 }
 
-export default connect(mapStateToProps)(BookList);
+// anything returned from this function will end up as props on the bookslist
+// container
+function mapDispatchToProps(dispatch) {
+  // when ever select book is call the result should be passed to all our
+  // reducers
+
+  return bindActionCreators({ selectBook }, dispatch);
+}
+
+// promote booklist from a component to a container - it needs to know about theis new
+// disapatch method selectBook. make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'JavaScript the Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Enders Game' },
+    { title: 'JavaScript the Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 88 },
+    { title: 'The Dark Tower', pages: 55 },
+    { title: 'Enders Game', pages: 1 },
   ];
 }
